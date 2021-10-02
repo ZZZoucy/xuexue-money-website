99b2f06f0328655cabd3cc0e171565fb8a899337
xiaozou <1424113865@qq.com>
2021-10-03 00:28:13 +0800

    deploy

---
+++ precache-manifest.409928b50b8375f85de6d45ee0f81991.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12bd5dedd434ec70813c",
+    "url": "/xuexue-money-website/css/app.db5611a3.css"
+  },
+  {
+    "revision": "35b0584a87fc490ba7c7dee75baaead8",
+    "url": "/xuexue-money-website/index.html"
+  },
+  {
+    "revision": "12bd5dedd434ec70813c",
+    "url": "/xuexue-money-website/js/app.a147ea97.js"
+  },
+  {
+    "revision": "a7b6282dee3cf1883294",
+    "url": "/xuexue-money-website/js/chunk-vendors.3df0ed18.js"
+  },
+  {
+    "revision": "dabf655311b1408d2798f4f58c7fca7e",
+    "url": "/xuexue-money-website/manifest.json"
+  },
+  {
+    "revision": "278e32771c83a6f6c0d6720edf1961fd",
+    "url": "/xuexue-money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/xuexue-money-website/robots.txt"
+  }
+]);
